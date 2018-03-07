26ec73080bdf9c1c887ffff9da988771d554d29a
Jennifer Marx <jennyrutledge@gmail.com>
2018-03-06 21:49:07 -0500

    Grab all associated attributes for pivot_facets.

--- refinery/user_files_manager/utils.py
+++ refinery/user_files_manager/utils.py
@@ -1,7 +1,10 @@
+from django.conf import settings
 from django.contrib.auth.models import User
 
 from core.utils import get_resources_for_user
-from data_set_manager.models import Assay, Study
+from data_set_manager.models import Assay, AttributeOrder, Study
+from data_set_manager.search_indexes import NodeIndex
+from data_set_manager.serializers import AttributeOrderSerializer
 from data_set_manager.utils import generate_solr_params
 
 
@@ -30,6 +33,7 @@ def generate_solr_params_for_user(params, user_id):
     datasets = get_resources_for_user(user, "dataset")
 
     assay_uuids = []
+
     for dataset in datasets:
         investigation_link = dataset.get_latest_investigation_link()
         if investigation_link is None:
@@ -45,6 +49,47 @@ def generate_solr_params_for_user(params, user_id):
         assay_uuids += Assay.objects.filter(
             study_id__in=study_ids
         ).values_list('uuid', flat=True)
+        # Missing facet_fields, it is generated from Attribute Order Model.
+        attributes_str = AttributeOrder.objects.filter(
+            assay__uuid__in=assay_uuids
+        )
+
+        attributes = AttributeOrderSerializer(attributes_str, many=True)
+
+        facet_template_char = '{0}_Characteristics{1}'
+        facet_template_fac = '{0}_Factor_Value{1}'
+
+        config_facets = settings.USER_FILES_FACETS.split(",")
+        pivot_facets = {}
+
+        # filter though attributes & keep the user files related attribute name
+        for facet_name in config_facets:
+            facet_char_generic_name = facet_template_char.format(
+                facet_name,
+                NodeIndex.GENERIC_SUFFIX
+            )
+            facet_fac_generic_name = facet_template_fac.format(
+                facet_name,
+                NodeIndex.GENERIC_SUFFIX
+            )
+            pivot_facets[facet_char_generic_name] = []
+            pivot_facets[facet_fac_generic_name] = []
+            # check if config facet needs a pivot
+            # { org_char_generic: [org_char_1, org_char_2],
+            # type_char_generic: [type_char_1, type_char_2]  }
+            for attribute_obj in attributes.data:
+                attribute_name = attribute_obj.get('solr_field')
+                if attribute_name.startswith(''.join(
+                        [facet_name, '_Characteristics']
+                )) and attribute_name != facet_char_generic_name:
+                    pivot_facets[facet_char_generic_name].append(
+                        attribute_obj.get('solr_field')
+                    )
+                if attribute_name.startswith(''.join(
+                        [facet_name, '_Factor_Value']
+                )) and attribute_name != facet_fac_generic_name:
+                    pivot_facets[facet_fac_generic_name].append(
+                        attribute_obj.get('solr_field'))
 
     return generate_solr_params(params,
                                 assay_uuids=assay_uuids,
